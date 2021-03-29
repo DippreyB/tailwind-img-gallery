84e80d916804c5fa13332581048cf826f81e1763
DippreyB <dippreyb@lisd.net>
2021-03-29 14:42:58 -0500

    Done with tutorial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import React, {useState, useEffect} from 'react';
+import { ImageCard } from './components/ImageCard';
+import { SearchForm } from './components/SearchForm';
 
 function App() {
+
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  const handleTermChange = (newTerm) => {
+    setTerm(newTerm);
+  }
+
+  useEffect( () => { 
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+    .then(res => res.json())
+    .then(data => {
+      setImages(data.hits);
+      setIsLoading(false);
+    })
+    .catch(err => console.log(err));
+  },[term])
+
   return (
-    <div>Hi</div>
+   
+    <div className="mx-auto container">
+      <h1 className="text-4xl text-purple-500 text-center mt-4 font-sans">Pixabay API Search</h1>
+      <SearchForm handleTermChange={handleTermChange} />
+      {isLoading ? 
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading</h1> : 
+        <div className="grid grid-cols-3 gap-4">
+          { images.map(img => (
+            <ImageCard key={img.id} img={img}/>
+            ))
+          }
+      </div>
+     }
+
+    </div>
   );
 }
 
---
+++ src/components/ImageCard.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+export const ImageCard = ({img}) => {
+    const tags = img.tags.split(",");
+    return (
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img className="w-full" src={img.webformatURL} alt=""/>
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by {img.user}
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>
+             {img.views}
+          </li>
+          <li>
+            <strong>Downloads: </strong>
+             {img.downloads}
+          </li>
+          <li>
+            <strong>Likes: </strong>
+             {img.likes}
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 py-4">
+          {tags.map(tag => (
+              <span key={img.id+tag} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mt-2">
+                #{tag}
+            </span>
+            ))
+          }
+      </div>
+    </div>
+    )
+}
